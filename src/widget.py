10d63355d00656b167b7d1013dd3947f14dd497a
Alexander Schischkin <schischkin2@yandex.com>
2026-02-09 00:05:43 +0100

    change imports

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from src.masks import get_mask_account, get_mask_card_number
+import src.masks
 
 
 def mask_account_card(input_string: str) -> str:
@@ -19,14 +19,14 @@ def mask_account_card(input_string: str) -> str:
     if "Счет" in input_string:
         parts = input_string.split()
         account_number = parts[-1]
-        masked_number = get_mask_account(account_number)
+        masked_number = src.masks.get_mask_account(account_number)
         return f"Счет {masked_number}"
     else:
         parts = input_string.split()
         card_type = " ".join(parts[:-1])
         card_number = parts[-1]
         try:
-            masked_number = get_mask_card_number(card_number)
+            masked_number = src.masks.get_mask_card_number(card_number)
         except ValueError:
             raise ValueError("Неверный формат номера карты")
         return f"{card_type} {masked_number}"
